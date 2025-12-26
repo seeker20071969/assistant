36a5eebbeb6285caac30e67b1cd2bb2982f52735
seeker20071969 <seeker20071969@proton.me>
2025-12-26 23:09:48 +0100

    Add OpenAI API for prompting

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import chainlit as cl
 from openai import OpenAI
 
-#client = OpenAI()
+client = OpenAI()
 
 @cl.on_message
 async def main(message: cl.Message):
 
-    '''
+
     response = client.chat.completions.create(
         model="gpt-4o-mini",
         messages=[
@@ -16,6 +16,5 @@ async def main(message: cl.Message):
     )
 
     reply = response.choices[0].message.content
-    '''
 
-    await cl.Message(content=message.content).send()
+    await cl.Message(content=reply).send()
